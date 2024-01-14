79eee7b2ec1e414ec5530039ba10b8c40a659e5c
dufedanceq <103373187+dufedanceq@users.noreply.github.com>
2024-01-14 23:53:14 +0200

    replaced let array with const cease

--- Tasks/split.js
+++ Tasks/split.js
@@ -4,9 +4,8 @@
 
 const split_Array = (index, array) => {
   const begin = array.slice(0, index);
-  const len = array.length;
-  array = array.slice(index, len);
-  return [begin, array];
+  const cease = array.slice(index, array.length);
+  return [begin, cease];
 };
 
 module.exports = split_Array;
