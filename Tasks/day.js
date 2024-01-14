c46dcc034ef3f679b5f201493dab0d9893e19965
dufedanceq <103373187+dufedanceq@users.noreply.github.com>
2024-01-14 22:16:17 +0200

    rename variables

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,15 @@
 // Get day number
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+let Days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
+parseDay = (strt) => {
   let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+  for (i = 0; i < Days.length; i++) {
+    if (strt.startsWith(Days[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
