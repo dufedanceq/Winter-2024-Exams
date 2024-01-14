b049ac9f4a5d50ce700082420e0165bf05f98bc0
dufedanceq <103373187+dufedanceq@users.noreply.github.com>
2024-01-14 22:58:25 +0200

    const and let

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,9 +1,9 @@
 // Sum all number values in dict
 
-count = (obj) => {
+const count = (obj) => {
   
-  sum = 0;
-  keys = Object.keys(obj);
+  let sum = 0;
+  const keys = Object.keys(obj);
   
   keys.forEach((key) => {
     const value = obj[key];
