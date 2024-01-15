ed11c26fff13b029729a60f0a44484dcb1be9ce2
dufedanceq <103373187+dufedanceq@users.noreply.github.com>
2024-01-15 11:05:14 +0200

    add const and 'use strict'

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,8 +1,10 @@
 // Get month number
 
-months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec',];
+'use strict'
 
-getMonthNum = (monthInput) => {
+const months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec',];
+
+const getMonthNum = (monthInput) => {
   for (let i = 0; i < months.length; i++) {
     if (monthInput.toLowerCase().startsWith(months[i])) return i + 1;
   }
