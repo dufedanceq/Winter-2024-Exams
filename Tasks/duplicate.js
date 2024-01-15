9ca647c8e918455b012903fdf55c6c23053ec5ce
dufedanceq <103373187+dufedanceq@users.noreply.github.com>
2024-01-15 09:39:43 +0200

    rename

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,13 +1,13 @@
 // Return an array without duplicates
 
-duplicate = (value, N) => {
-  if (N <= 0) return [];
+duplicate = (value, numberOfRepeats) => {
+  if (numberOfRepeats <= 0) return [];
   else {
-    res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
+    result = [];
+    for (let i = 0; i < numberOfRepeats; i++) {
+      result[i] = value;
     }
-    return res;
+    return result;
   }
 };
 
