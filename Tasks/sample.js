dd50e7e4d6d2385af190d07baf95282e30b5e46f
dufedanceq <103373187+dufedanceq@users.noreply.github.com>
2024-01-15 09:17:39 +0200

    rename once again

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,8 +1,8 @@
 // Get one random element from an array
 
-const getRandomElement = (arr) => {
-  arr = arr[Math.floor(Math.random() * arr.length)];
-  return arr;
+const getRandomElement = (array) => {
+  array = array[Math.floor(Math.random() * array.length)];
+  return array;
 };
 
 module.exports = getRandomElement;
