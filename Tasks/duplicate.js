a9fe15152f141a85395c648d602f7eb8102a4807
dufedanceq <103373187+dufedanceq@users.noreply.github.com>
2024-01-15 09:40:53 +0200

    change structure

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -2,13 +2,11 @@
 
 duplicate = (value, numberOfRepeats) => {
   if (numberOfRepeats <= 0) return [];
-  else {
     result = [];
     for (let i = 0; i < numberOfRepeats; i++) {
       result[i] = value;
     }
     return result;
-  }
 };
 
 module.exports = duplicate;
